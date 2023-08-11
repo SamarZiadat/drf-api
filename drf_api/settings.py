2c26b5b0c80faae05b7f4c1f9b56a8dab667b06c
SamarZiadat <samarziadat@gmail.com>
2023-08-11 10:16:51 +0000

    fix: remove hard coding of allowed hosts in settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -70,7 +70,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drf-api-samar-918d34c4a809.herokuapp.com']
+ALLOWED_HOSTS = [
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
+]
 
 # Application definition
 
